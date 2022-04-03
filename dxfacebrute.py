4be3693ef7572437c6f099ee34c91ab1215382b5
Daniel Derefaka <101010297+DanielDerefaka@users.noreply.github.com>
2022-04-03 14:26:29 +0100

    Create dxfacebrute.py

---
+++ dxfacebrute.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# This Cooding is Coode in python 2 now it willbe renew soon
+
+import sys
+import mechanize
+import cookielib
+import random
+
+#pip2 install mechanize
+#pip2 install requests
+
+
+
+@@ -60,14 +62,14 @@ def search():
+		brute(password)
+
+
+#welcome 
+#welcome Dx Hackers 
+def welcome():
+	wel = """
+        +=========================================+
+        |..........   Facebook Brute Dx  .........|
+        |..........   Facebook = Brute   .........|
+        +-----------------------------------------+
+        |            #Author: Daniel Derefaka     | 
+        |	       Version 1.0                      |
+        |            #Author: Daniel Derefaka     | 
+        |	       Version 1.0                      |
+ 	      |    Contact = +2348166271623             |
+        +=========================================+
+        |..........   DxFace brute     ...........|
